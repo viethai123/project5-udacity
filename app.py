aa0c95c4464e08e441e087022ef8507da1648c47
haringuyen <viethai180199@gmail.com>
2023-06-13 02:03:09 +0700

    update code

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
  
 app = Flask(__name__)
  
-@app.route("/hello", methods=["GET"])
-def hello():
+@app.route("/helloWorld", methods=["GET"])
+def helloWorld():
     return 'Hello World!'
  
 app.run(host="0.0.0.0", port=80, debug=True)
